d6481e2a8264daea8a300640a27211787a8a28ea
shurizzle <shura1991@gmail.com>
2023-08-15 18:15:23 +0200

    Fix paste.rs provider (#13)

--- lua/paperplanes/providers/pasters.lua
+++ lua/paperplanes/providers/pasters.lua
@@ -1,11 +1,18 @@
- local function provide(content, metadata, opts, on_complete)
- local curl = require("paperplanes.curl")
- local args = {"--data-binary", content, "https://paste.rs"} local resp_handler
- local function _1_(response, status)
- local _2_ = status if (_2_ == 201) then
-
- return on_complete(string.match(response, "^(https?://.*)\n")) elseif true then local _ = _2_
- return on_complete(nil, response) else return nil end end resp_handler = _1_
- return curl(args, resp_handler) end
-
- return provide
+local function provide(content, metadata, opts, on_complete)
+  local curl = require("paperplanes.curl")
+  local args = {"--data-binary", content, "https://paste.rs"}
+  local resp_handler
+  local function _1_(response, status)
+    if (status == 201) then
+      return on_complete(response)
+    elseif true then
+      local _ = status
+      return on_complete(nil, response)
+    else
+      return nil
+    end
+  end
+  resp_handler = _1_
+  return curl(args, resp_handler)
+end
+return provide
