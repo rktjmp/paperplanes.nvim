351b5f067b30b944f0554e870da512c4e77f04d6
Oliver Marriott <hello@omarriott.com>
2025-07-11 23:55:44 +1000

    chore: bump build

--- lua/paperplanes/providers/termbin.lua
+++ lua/paperplanes/providers/termbin.lua
@@ -47,10 +47,8 @@ local function create(content, _content_metadata, options, on_complete)
   local command = find_command(options)
   local host = (options.host or "termbin.com")
   local port = (options.port or "9999")
-  local filename = vim.fn.tempname()
   local on_exit
   local function _10_(exit_code, output, errors)
-    uv.fs_unlink(filename)
     if (exit_code == 0) then
       local _11_ = string.match(output, "(.+)\n.*")
       if (nil ~= _11_) then
@@ -68,53 +66,11 @@ local function create(content, _content_metadata, options, on_complete)
   end
   on_exit = _10_
   local on_spawn
-  local function _14_(process)
-    local stdin = process["stdin"]
-    for line in io.lines(filename) do
-      uv.write(stdin, (line .. "\n"))
-    end
-    return nil
+  local function _15_(_14_)
+    local stdin = _14_["stdin"]
+    return uv.write(stdin, content)
   end
-  on_spawn = _14_
-  do
-    local outfile = io.open(filename, "w")
-    local function close_handlers_12_(ok_13_, ...)
-      outfile:close()
-      if ok_13_ then
-        return ...
-      else
-        return error(..., 0)
-      end
-    end
-    local function _16_()
-      return outfile:write(content)
-    end
-    local _18_
-    do
-      local t_17_ = _G
-      if (nil ~= t_17_) then
-        t_17_ = t_17_.package
-      else
-      end
-      if (nil ~= t_17_) then
-        t_17_ = t_17_.loaded
-      else
-      end
-      if (nil ~= t_17_) then
-        t_17_ = t_17_.fennel
-      else
-      end
-      _18_ = t_17_
-    end
-    local or_22_ = _18_ or _G.debug
-    if not or_22_ then
-      local function _23_()
-        return ""
-      end
-      or_22_ = {traceback = _23_}
-    end
-    close_handlers_12_(_G.xpcall(_16_, or_22_.traceback))
-  end
+  on_spawn = _15_
   return exec(command, {host, port}, on_exit, on_spawn)
 end
 return {create = create, completions = completions}
